f3eee7025465617c2c8d0b3b05322fb5a8e39013
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-19 15:13:37 +0100

    Refactor → added comments to 1st Snack JavaScript

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ console.log('JS Test')
 
 /// ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ Mie Funzioni ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ \\\
 
-
+// funzione per la creazione dell'oggetto contenente i dati relativi al vip
 function generateTableObject(tableName, guestName, guestSeat){
     const Object = {
         nomeTavolo: tableName,
@@ -23,18 +23,18 @@ function generateTableObject(tableName, guestName, guestSeat){
 console.log(' ')
 console.log('__________________________| 1° Snack |__________________________')
 
+
+// Array di partenza
 const TavoloVip =  ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
 
 
-
-
-
+// ciclo for che consete dato l'array, di stampare un oggetto contenente i dati richiesti
 for(i = 0; i < TavoloVip.length; i++){
     const vip = TavoloVip[i]
     const posto = TavoloVip.indexOf(vip) + 1
 
-    const ciao = generateTableObject('Tavolo Vip', vip, posto)
-    console.log(ciao)
+    const vipInfo = generateTableObject('Tavolo Vip', vip, posto)
+    console.log(vipInfo)
 }
 
 
