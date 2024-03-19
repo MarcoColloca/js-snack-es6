10e0efbbb322e83b365e385817bddc4361f7557c
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-19 15:36:23 +0100

    Feat → added console generation of student lists

--- js/app.js
+++ js/app.js
@@ -51,3 +51,73 @@ for(i = 0; i < TavoloVip.length; i++){
 console.log(' ')
 console.log(' ')
 console.log('__________________________| 2° Snack |__________________________')
+
+
+// Array degli studenti
+const studenti = [
+    {
+        Id: 213,
+        Name: 'Marco della Rovere',
+        Grades: 78
+    },
+    {
+        Id: 110,
+        Name: 'Paola Cortellessa',
+        Grades: 96
+    },
+    {
+        Id: 250,
+        Name: 'Andrea Mantegna',
+        Grades: 48
+    },
+    {
+        Id: 145,
+        Name: 'Gaia Borromini',
+        Grades: 74
+    },
+    {
+        Id: 196,
+        Name: 'Luigi Grimaldello',
+        Grades: 68
+    },
+    {
+        Id: 102,
+        Name: 'Piero della Francesca',
+        Grades: 50
+    },
+    {
+        Id: 120,
+        Name: 'Francesca da Polenta',
+        Grades: 84
+    },
+]
+
+
+const studentsNames = [];
+
+const studentMinGrade = [];
+
+const studentMinGradeId = [];
+
+
+
+for(i = 0; i < studenti.length; i++){
+
+    const studentName = studenti[i].Name.toUpperCase()
+    studentsNames.push(studentName)
+
+    if(studenti[i].Grades > 70){
+        studentMinGrade.push(studenti[i])
+    }
+    
+    if ((studenti[i].Grades > 70 && studenti[i].Id > 120)){
+        studentMinGradeId.push(studenti[i])
+    }
+}
+
+console.log(studentsNames)
+console.log(studentMinGrade)
+console.log(studentMinGradeId)
+
+
+const studentListDOMElement = document.querySelector('.student-list')
